8feb161fecebe537c385ed55ee5ed9c47a451fb9
roshith93 <r.roshith@gmail.com>
2019-06-20 18:50:04 +0530

    title resuable component added

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 import Product from './Product'
+import Title from './Title';
 
 class ProductList extends Component {
   render(){
     return (
-        <Product />
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="product" />
+            <div className="row">
+              
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+        // <Product />
   );
 }
 }
